66b5a7eb757e79884a21cae13665a754d8acaa5b
Akash Das <akashd595@gmail.com>
2022-05-25 22:28:09 +0530

    ADDED: master

---
+++ Gambler.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.day07_gambling;
+
+public class Gambler {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the master of Gambler");
+    }
+}
